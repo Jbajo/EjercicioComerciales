c34fda3768343836bd402673ed75b480d9a3cc98
Javier Bajo <javier.bajochacon@gmail.com>
2017-12-29 08:54:18 +0100

    Terminado

--- src/main/java/com/example/demo/ComercialApplication.java
+++ src/main/java/com/example/demo/ComercialApplication.java
@@ -19,10 +19,7 @@ public class ComercialApplication {
 		String apellido ="";
 		Scanner sc = new Scanner (System.in);
 		ArrayList <Comercial> comerciales = new ArrayList <Comercial>();
-		
-		
-		
-		
+				
 		comerciales.add(new Comercial(1234, "Paco", "Rodriguez", 1600));
 		comerciales.add(new Comercial(1234, "Francisco", "Rodriguez", 1700));
 		Venta venta1 =comerciales.get(0).crearVentaParametro(1236, "tomate", 170);				
@@ -30,9 +27,7 @@ public class ComercialApplication {
 		comerciales.get(0).mostrarListaVentas();
 		comerciales.get(1).mostrarListaVentas();		
 		comerciales.get(0).devolucion(1236);	
-		comerciales.get(1).devolucion(1235);
-		
-		
+		comerciales.get(1).devolucion(1235);		
 		
 		
 	}
